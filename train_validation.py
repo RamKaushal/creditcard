f5b1f5e3f454a00cd4856651f51d26e9dc163971
ram kaushal <ramkaushalguralla@gmail.com>
2022-03-04 12:46:44 +0530

    rool back commit

--- train_validation.py
+++ train_validation.py
@@ -4,17 +4,19 @@ import os
 import pandas as pd
 import shutil
 class train_validator:
-    def __init__(self,path,config_path,archive_path,correct_path):
+    #def __init__(self,path,config_path,archive_path,correct_path):
+    def __init__(self,path):
         self.path = path 
-        self.archive_path = archive_path
-        self.correct_path = correct_path
-        self.config_path = config_path
         log = mylogger()
     def valid(self):
         for i in os.listdir(self.path):
+            print(i)
+            """
             count = 0
             full_path = self.path+'\\'+i
             raw_data = pd.read_csv(self.path+'\\'+i)
+            print(raw_data.head(5))
+            
             con =  open(self.config_path).read()
             configs = eval(con)
             dtypes = raw_data.dtypes 
@@ -33,9 +35,10 @@ class train_validator:
                             print("{} if a corrrupted file at the coulmn {}".format(full_path,configs['ColName'][i]))
                             #shutil.move(full_path,self.archive_path)
                             break 
+            """
           
 if __name__ == '__main__':
-     trail = train_validator("C:\\Users\\ramka\\Desktop\\Creditcard\\data","C:\\Users\\ramka\\Desktop\\Creditcard\\schema_training.json","C:\\Users\\ramka\\Desktop\\Creditcard\\data\\Archive","C:\\Users\\ramka\\Desktop\\Creditcard\\data\\correct_files") 
+     trail = train_validator("C:\\Users\\ramka\\Desktop\\Creditcard\\data") 
      trail.valid()               
                 
 
